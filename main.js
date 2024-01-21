6e653579b924067a8736c462e18faf320ec8a491
Aisha Saad <111194434+Aisha-Saad@users.noreply.github.com>
2024-01-21 08:43:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+
+
+let menuIcon = document.querySelector("#menu-icon");
+
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = (eo) => {
+    menuIcon.classList.toggle("fa-x");
+    navbar.classList.toggle("active");
+    eo.preventDefault()
+};
+
+
+
+
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+
+
+
+
+
+            });
+        };
+
+    });
+
+
+    let header = document.querySelector('header');
+
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+
+
+
+    menuIcon.classList.remove('fa-x');
+    navbar.classList.remove('active');
+
+
+
+
+};
+
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+
+
+
+});
+
+ScrollReveal().reveal('.main-home, .home-text', { origin: 'top' });
+ScrollReveal().reveal('.home, .services-continar, .education',{origin:'bottom'});
+
